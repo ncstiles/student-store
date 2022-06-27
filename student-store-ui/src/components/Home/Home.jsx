46d13e12187d3a8d7185815d8e78e79998fa9fa3
ncstiles <ncstiles17@gmail.com>
2022-06-27 16:03:18 -0700

    Implemented basic scaffolding with routing to main components.  Props and states created, all product data pulled via Axios GET request.

--- student-store-ui/src/components/Home/Home.jsx
+++ student-store-ui/src/components/Home/Home.jsx
@@ -1,10 +1,43 @@
 import * as React from "react"
 import "./Home.css"
+import { useState } from "react";
 
-export default function Home() {
+import Hero from "../Hero/Hero"
+import ProductGrid from "../ProductGrid/ProductGrid"
+import About from "../About/About";
+import Contact from "../Contact/Contact";
+
+import Search from '../Search/Search';
+export default function Home({ products, shoppingCart, handleAddItemToCart, handleRemoveItemToCart }) {
+  let [filterCategory, setFilterCategory] = useState('all')
+  let [filteredProducts, setFilteredProducts] = useState([])
+  let [searchInput, setSearchInput] = useState('')
+  let [searchResults, setSearchResults] = useState([])
+
   return (
-    <div className="home">
-      <p>Home</p>
-    </div>
+      <div id='home-top' className="home">
+        <header><Hero className='hero'/></header>
+    
+        <div id='content-top' className='contents'>
+          <Search products={products} 
+                  filterCategory={filterCategory}
+                  setFilterCategory={setFilterCategory}
+                  filteredProducts={filteredProducts}
+                  setFilteredProducts={setFilteredProducts}
+                  searchInput={searchInput}
+                  setSearchInput={setSearchInput}
+                  searchResults={searchResults}
+                  setSearchResults={setSearchResults}/>
+
+            <ProductGrid  products={products} 
+                          filteredProducts={filteredProducts}
+                          searchResults = {searchResults}
+                          shoppingCart={shoppingCart} 
+                          handleAddItemToCart={handleAddItemToCart} 
+                          handleRemoveItemToCart={handleRemoveItemToCart}/>
+          <About/>
+          <div id='contact'><Contact/></div>
+        </div>  
+      </div>    
   )
 }
